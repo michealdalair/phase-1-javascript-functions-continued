489c83b9e474c379011c894cadc7b08e5b81a990
michaljones <micheal.jones2010@yahoo.com>
2023-11-05 13:46:08 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+
+
+   const mondayWork= function(activity="go to the office"){
+    if(activity){
+        return `This Monday, I will ${activity}.`;
+
+    }else{
+    return "This Monday, I will work from home."
+    }
+   
+
+
+   }
+
+
+function wrapAdjective(string= "*"){
+    return function(name="special"){
+        return `You are ${string}${name}${string}!`
+    }
+        
+}
+
+     
+wrapAdjective("%")("a dedicated programmer"); //=> "You are %a dedicated programmer%!" 
+   
